92945b769066e6153ff5f43293391d2a13380096
chris <dwatow@gmail.com>
2018-12-24 17:10:56 +0800

    Hello world AngularJS

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-console.log('main');
+import angular from 'angular';
+
+class controller{
+  constructor() {
+    console.log('constructor');
+  }
+}
+
+angular.module('MainApp', [])
+
+angular.module('app', [])
+  .component('main', {
+    template: `<div>Hello AngularJS</div>`,
+    controller,
+    controllerAs: 'vm',
+  }).name;
