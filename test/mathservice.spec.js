d38b8e001aada39de5947680f7ca73a51d779927
chris <dwatow@gmail.com>
2019-08-11 19:30:16 +0800

    add angularjs-jest

--- test/mathservice.spec.js
+++ test/mathservice.spec.js
@@ -3,33 +3,31 @@ import 'angular-mocks';
 
 import mathservice from '../src/service/mathmodule.js';
 import helloWorld from '../src/components/HelloWorld/index.js';
-
-beforeEach(() => {
-  angular.mock.module(mathservice);
-  angular.mock.module(helloWorld);
-});
-
-
-var __$componentController__  = null;
-beforeEach(inject(($log, mathservice, $componentController) => {
-  __$componentController__ = $componentController;
-}))
+import { createTestApp } from 'angularjs-jest';
 
 describe('helloWorld - component', function () {
-  it('initial is exist', () => {
-    const sut = __$componentController__(helloWorld)
-    expect(sut).toBeDefined();
+  const getTestApp = () => createTestApp({
+    modules: [helloWorld, mathservice],
+    access: ['$componentController'],
   });
 
+  it('initial is exist', () => {
+    const app = getTestApp();
+    const sut = app.$componentController(helloWorld);
+    expect(sut).toBeDefined();
+  });
+  
   it('initial binding msg: should be Welcome to Your AngularJS App', () => {
-    const sut = __$componentController__(helloWorld, null, {
+    const app = getTestApp();
+    const sut = app.$componentController(helloWorld, null, {
       msg: "Welcome to Your AngularJS App",
-    })
+    });
     expect(sut.msg).toEqual("Welcome to Your AngularJS App");
   });
   
   it('should go', () => {
-    const sut = __$componentController__(helloWorld);
+    const app = getTestApp();
+    const sut = app.$componentController(helloWorld);
     sut.$onInit();
     expect(sut.math).toEqual(3);
   });
